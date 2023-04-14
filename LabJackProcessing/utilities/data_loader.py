b1ab34571c8e5c92da970adc14c1278270adba9d
philipqueen <queen.philip@gmail.com>
2023-04-14 13:48:51 -0600

    More robust column name changes (still work to do)

--- LabJackProcessing/utilities/data_loader.py
+++ LabJackProcessing/utilities/data_loader.py
@@ -7,7 +7,7 @@ from LabJackProcessing.utilities.path_utilities import ensure_path_object
 
 logger = logging.getLogger(__name__)
 
-def load_data(file_path: Path):
+def load_data(file_path: Path) -> pd.DataFrame:
     file_path = ensure_path_object(file_path)
     logger.info(f"Opening {str(file_path)}...")
     
@@ -24,6 +24,23 @@ def load_data(file_path: Path):
 
     df = pd.read_csv(file_path, sep=sep, skiprows=rows_to_skip)
 
-    df.columns = ["Time", "Voltage1", "Voltage2", "Distance_Traveled", "Force_in_kN"]
+    force_column_name = 'Force_in_kN'
+    force_column_code = 'y2'
+    alternate_force_column_code = 'y1'
+    try:
+        rename_column(df=df, old_column_name=force_column_code, new_column_name=force_column_name)
+    except KeyError:
+        rename_column(df=df, old_column_name=alternate_force_column_code, new_column_name=force_column_name)
+    except:
+        logger.error(f"Failed to find force column in file.")
+        raise Exception("Failed to find force column in file.")
 
     return df
+
+def rename_column(df: pd.DataFrame, old_column_name: str, new_column_name: str):
+    if old_column_name in df.columns:
+        logger.info(f"Renaming {old_column_name} to {new_column_name}...")
+        df.rename(columns={old_column_name: new_column_name}, inplace=True)
+    else:
+        logger.error(f"Column {old_column_name} not found in file.")
+        raise KeyError(f"Column {old_column_name} not found in file.")
